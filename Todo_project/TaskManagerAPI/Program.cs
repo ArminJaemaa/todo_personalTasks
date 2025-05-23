53fb697e589acaf72c8756bcd55da5fff822154e
ArminJaemaa <ajaemaa@tlu.ee>
2025-05-23 15:39:53 +0300

    Andmebaasiga ühendus loodud, tehtud 2 table-it, User ja TaskList.

---
+++ Todo_project/TaskManagerAPI/Program.cs
@@ -0,0 +1,17 @@
+using TaskManagerAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Run();
